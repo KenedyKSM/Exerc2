155c88971913b14b8a68488c73cce5370441be78
Kenedy Madueke <kenedy.max17@gmail.com>
2025-10-26 21:13:29 -0300

    comentarios para os proximos exerc

--- Exerc3/Main.java
+++ Exerc3/Main.java
@@ -2,4 +2,11 @@ package Exerc3;
 
 public class Main {
     
+    public static void main(String[] args) {
+        
+    }
+
+    /*
+     Escreva um codigo que o usuario entre com um primeiro numero, um segundo numero maior que o primeiro e escolhe entre a opcao par e impar, com isso o codigo deve informar todos os numeros pares ou impares (de acordo com a selecao inicial) no intervalo de numeros informados, incluindo os numeros informados e em ordem decrescente;
+     */
 }
--- Exerc4/Main.java
+++ Exerc4/Main.java
@@ -2,4 +2,11 @@ package Exerc4;
 
 public class Main {
     
+    public static void main(String[] args) {
+        
+    }
+
+    /*
+     Escreva um codigo onde o usuario informa um numero inicial, posteriormente ira informar outros N numeros, a execucao do codigo ira continuar ate que o numero informado dividido pelo primeiro numero tenha resto diferente de 0 na divisao, numeros menores que o primeiro numero devem ser ignorados
+     */
 }
